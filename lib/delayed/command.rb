78f4bfbf9e9cda9abe761de30584cbd9a934e3e9
Patrick McKenzie <patrick@bingocardcreator.com>
2009-05-09 13:51:33 +0900

    Added ability to run multiple workers concurrently.

--- lib/delayed/command.rb
+++ lib/delayed/command.rb
@@ -21,46 +21,60 @@ module Delayed
         opts.on('-s', '--sleep=seconds', "Number of seconds between checking for new jobs") do |secs|
           @options.sleep = secs
         end
+        opts.on('-n', '--number_of_workers=workers', "Number of unique workers to spawn") do |worker_count|
+          @options.worker_count = worker_count.to_i rescue 1
+        end
       end
       @args = opts.parse!(args)
     end
   
     def run
-      Daemons.run_proc('delayed_job', :dir => "#{RAILS_ROOT}/tmp/pids", :dir_mode => :normal, :ARGV => @args) do |*args|
-        begin
-          require File.join(RAILS_ROOT, 'config', 'environment')
-          
-          # Replace the default logger
-          logger = Logger.new(File.join(RAILS_ROOT, 'log', 'delayed_job.log'))
-          logger.level = ActiveRecord::Base.logger.level
-          ActiveRecord::Base.logger = logger
-          ActiveRecord::Base.clear_active_connections!
-          
-          logger.info "*** Starting job worker #{Delayed::Job.worker_name}"
-          
-          trap('TERM') { puts 'Exiting...'; $exit = true }
-          trap('INT')  { puts 'Exiting...'; $exit = true }
-          
-          loop do      
-            result = nil
-            realtime = Benchmark.realtime { result = Delayed::Job.work_off }
-            count = result.sum
-    
-            break if $exit
-    
-            if count.zero? 
-              sleep @options.sleep
-              logger.debug 'Waiting for more jobs...'
-            else
-              logger.info "#{count} jobs processed at %.4f j/s, %d failed ..." % [count / realtime, result.last]
+      worker_count = @options.worker_count || 1
+
+      1.upto(worker_count) do |worker_index|
+        if (worker_count == 1)
+          process_name = "delayed_job"
+        else
+          process_name = "delayed_job.#{worker_index}"
+        end
+        Daemons.run_proc(process_name, :dir => "#{RAILS_ROOT}/tmp/pids", :dir_mode => :normal, :ARGV => @args) do |*args|
+          begin
+            require File.join(RAILS_ROOT, 'config', 'environment')
+
+            # Replace the default logger
+            logger = Logger.new(File.join(RAILS_ROOT, 'log', 'delayed_job.log'))
+            logger.level = ActiveRecord::Base.logger.level
+            ActiveRecord::Base.logger = logger
+            ActiveRecord::Base.clear_active_connections!
+            if (worker_count != 1)
+              Delayed::Job.worker_name = Delayed::Job.worker_name + " worker: #{worker_index}"
             end
-    
-            break if $exit
+            logger.info "*** Starting job worker #{Delayed::Job.worker_name}"
+
+            trap('TERM') { puts 'Exiting...'; $exit = true }
+            trap('INT')  { puts 'Exiting...'; $exit = true }
+
+            loop do
+              result = nil
+              realtime = Benchmark.realtime { result = Delayed::Job.work_off }
+              count = result.sum
+
+              break if $exit
+
+              if count.zero?
+                sleep @options.sleep
+                logger.debug 'Waiting for more jobs...'
+              else
+                logger.info "#{count} jobs processed at %.4f j/s, %d failed ..." % [count / realtime, result.last]
+              end
+
+              break if $exit
+            end
+          rescue => e
+            logger.fatal e
+            STDERR.puts e.message
+            exit 1
           end
-        rescue => e
-          logger.fatal e
-          STDERR.puts e.message
-          exit 1
         end
       end
     end
